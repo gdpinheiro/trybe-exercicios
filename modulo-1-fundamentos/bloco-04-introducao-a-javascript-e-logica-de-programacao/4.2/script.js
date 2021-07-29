eccd17a33f954fccf1e23f6e5581d987e8d13416
Giovanni Duarte <gikigiio@gmail.com>
2021-07-29 18:07:41 -0300

    Execução ex 1 e 2

---
+++ modulo-1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/script.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// exercício 1
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
+
+// exercício 2
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+console.log(soma);
