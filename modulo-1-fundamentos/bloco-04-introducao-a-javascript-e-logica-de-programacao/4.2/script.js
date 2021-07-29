179916416de3ee06a916f5621191c1bd39b063fa
Giovanni Duarte <gikigiio@gmail.com>
2021-07-29 18:10:11 -0300

    Execução ex 3

--- modulo-1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/script.js
+++ modulo-1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/script.js
@@ -1,14 +1,25 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// exercício 1
-for (let index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index]);
-}
+// // exercício 1
+// for (let index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index]);
+// }
 
-// exercício 2
+// // exercício 2
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+// }
+// console.log(soma);
+
+// exercício 3
 let soma = 0;
+let media = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   soma += numbers[index];
+  media = soma / numbers.length;
 }
 console.log(soma);
+console.log(media);
