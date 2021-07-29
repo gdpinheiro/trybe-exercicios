bd679336588d9e15f41748c56f8a77d6509c6ad0
Giovanni Duarte <gikigiio@gmail.com>
2021-07-29 18:28:44 -0300

    Execução ex 6

---
+++ modulo-1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/ex-6.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let isOdd = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 != 0) {
+    isOdd += 1;
+  }
+}
+
+console.log(isOdd);
