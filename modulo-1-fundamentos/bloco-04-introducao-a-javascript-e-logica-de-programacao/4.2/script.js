516152b7c978f31e1a19dbb3786a6d471cdf8d9d
Giovanni Duarte <gikigiio@gmail.com>
2021-07-29 18:14:54 -0300

    Execução ex 4

--- modulo-1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/script.js
+++ modulo-1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.2/script.js
@@ -13,13 +13,28 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 // console.log(soma);
 
-// exercício 3
-let soma = 0;
+// // exercício 3
+// let media = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   let soma = 0;
+//   soma += numbers[index];
+//   media = soma / numbers.length;
+// }
+// console.log(media);
+
+// exercício 4
 let media = 0;
+let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   soma += numbers[index];
   media = soma / numbers.length;
 }
-console.log(soma);
+if (media > 20) {
+  console.log("Valor maior que 20");
+} else {
+  console.log("Valor menor ou igual a 20");
+}
+
 console.log(media);
