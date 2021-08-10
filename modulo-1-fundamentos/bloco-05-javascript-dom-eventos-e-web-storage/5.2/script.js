5a10b9c943a48307c66e6ebc60dc681271bd47f4
Giovanni Duarte <gikigiio@gmail.com>
2021-08-10 18:51:03 -0300

    Execução Parte 2 Ex 3

--- modulo-1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/5.2/script.js
+++ modulo-1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/5.2/script.js
@@ -104,6 +104,10 @@ for (let i = 0; i < descriptions.length; i += 1) {
 
 // 3. Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
 
+let deletion = document.querySelector(".left-content");
+
+document.querySelector(".main-content").removeChild(deletion);
+
 // 4. Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
 
 // 5. Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
