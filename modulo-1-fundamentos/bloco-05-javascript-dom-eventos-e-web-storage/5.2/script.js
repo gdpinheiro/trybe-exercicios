421c5a96b328c0f0f62339625c3ba1b196f327a4
Giovanni Duarte <gikigiio@gmail.com>
2021-08-10 18:11:25 -0300

    Execução Ex 2

--- modulo-1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/5.2/script.js
+++ modulo-1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/5.2/script.js
@@ -6,6 +6,12 @@ document.body.appendChild(h1Ex1);
 
 // 2. Adicione a tag main com a classe main-content como filho da tag body ;
 
+let mainEx2 = document.createElement("main");
+
+document.body.appendChild(mainEx2);
+
+mainEx2.className = "main-content";
+
 // 3. Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 
 // 4. Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
