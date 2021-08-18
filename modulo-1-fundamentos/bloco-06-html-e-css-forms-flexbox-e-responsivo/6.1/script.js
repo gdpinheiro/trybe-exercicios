12df96dd431e1e3e59331d4c7602dd2ebe043e22
Giovanni Duarte <gikigiio@gmail.com>
2021-08-18 20:21:19 -0300

    Ex 3.2

--- modulo-1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/6.1/script.js
+++ modulo-1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/6.1/script.js
@@ -2,6 +2,23 @@ window.onload = function () {
   gerarEstados();
 };
 
+// Variáveis
+let emailInput = document.getElementById("input-email");
+let dataInput = document.getElementById("input-data");
+let campos = document.getElementsByClassName("campos");
+let dadosValidos = false;
+
+// Add Event Listeners
+function addListeners(targetElement, targetEvent, targetFunction) {
+  let elementArray = document.querySelectorAll(targetElement);
+  for (let i = 0; i < elementArray.length; i += 1) {
+    document
+      .querySelectorAll(targetElement)
+      [i].addEventListener(targetEvent, targetFunction);
+  }
+}
+
+// Gerar Estados
 const listaEstados = [
   "Acre",
   "Alagoas",
@@ -70,4 +87,73 @@ function gerarEstados(estado, sigla) {
     estadoNovo.setAttribute("value", siglaEstados[i]);
     select.appendChild(estadoNovo);
   }
+}
+
+// Verificar Vazios
+function verificarVazios(input) {
+  for (let i = 0; i < campos.length; i += 1) {
+    if (campos[i].value === "") {
+      window.alert("O campo " + campos[i].name + " é inválido");
+    } else {
+      dadosValidos = true;
+    }
+  }
+}
+
+// Verificar Email
+
+function verificarEmail(email) {
+  let emailPattern = new RegExp(/(.*)(\@)(.*)/);
+  let check = emailInput.value.match(emailPattern);
+  let validEmail = false;
+
+  if (check !== null) {
+    if (check[2] === true) {
+      validEmail = true;
+    }
+  } else {
+    window.alert("Email Inválido");
+  }
+}
+
+//Verificar Data
+
+function verificarData(data) {
+  let datePattern = new RegExp(/(\d{1,2})\/(\d{1,2})\/(\d{4})/);
+  let check = dataInput.value.match(datePattern);
+  let validData = false;
+
+  if (check !== null) {
+    if (
+      verificarNumero(check[1], 0, 32) === true &&
+      verificarNumero(check[2], 0, 12) === true &&
+      check[3] > 1
+    ) {
+      validData = true;
+    } else {
+      window.alert("Formato de Data Inválido");
+    }
+  } else {
+    window.alert("Formato de Data Inválido");
+  }
+}
+
+// Verificar Números
+
+function verificarNumero(num, min, max) {
+  if (num > min && num < max) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Botão de Enviar
+addListeners("#btn-enviar", "click", btnActions);
+
+function btnActions(elem) {
+  elem.preventDefault();
+  verificarVazios();
+  verificarEmail();
+  verificarData();
 }
