768fae04d6700bf1ed83e9961ce21881f0ad49f8
Giovanni Duarte <gikigiio@gmail.com>
2021-08-18 16:06:56 -0300

    Execução Ex 1

---
+++ modulo-1-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/6.1/script.js
@@ -0,0 +1,73 @@
+window.onload = function () {
+  gerarEstados();
+};
+
+const listaEstados = [
+  "Acre",
+  "Alagoas",
+  "Amapá",
+  "Amazonas",
+  "Bahia",
+  "Ceará",
+  "Distrito Federal",
+  "Espírito Santo",
+  "Goiás",
+  "Maranhão",
+  "Mato Grosso",
+  "Mato Grosso do Sul",
+  "Minas Gerais",
+  "Pará",
+  "Paraíba",
+  "Paraná",
+  "Pernambuco",
+  "Piauí",
+  "Rio de Janeiro",
+  "Rio Grande do Norte",
+  "Rio Grande do Sul",
+  "Rondônia",
+  "Roraima",
+  "Santa Catarina",
+  "São Paulo",
+  "Sergipe",
+  "Tocantins",
+];
+const siglaEstados = [
+  "AC",
+  "AL",
+  "AP",
+  "AM",
+  "BA",
+  "CE",
+  "DF",
+  "ES",
+  "GO",
+  "MA",
+  "MT",
+  "MS",
+  "MG",
+  "PA",
+  "PB",
+  "PR",
+  "PE",
+  "PI",
+  "RJ",
+  "RN",
+  "RS",
+  "RO",
+  "RR",
+  "SC",
+  "SP",
+  "SE",
+  "TO",
+];
+
+function gerarEstados(estado, sigla) {
+  let select = document.getElementById("select-estado");
+
+  for (let i = 0; i < listaEstados.length; i += 1) {
+    let estadoNovo = document.createElement("option");
+    estadoNovo.innerText = listaEstados[i];
+    estadoNovo.setAttribute("value", siglaEstados[i]);
+    select.appendChild(estadoNovo);
+  }
+}
