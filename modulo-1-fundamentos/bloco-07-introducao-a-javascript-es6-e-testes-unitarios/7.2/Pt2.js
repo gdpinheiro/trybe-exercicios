9aa7218e060aa86d17aac0e6bb3d1ebf8f32e15b
Giovanni Duarte <gikigiio@gmail.com>
2021-08-30 18:16:09 -0300

    Pt2E1

--- modulo-1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/Pt2.js
+++ modulo-1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/Pt2.js
@@ -17,3 +17,21 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+// 1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+const addKeyValue = (object, key, value) => {
+  let newKey = key
+  object[newKey] = value
+}
+
+addKeyValue(lesson2, 'turno', 'manhã')
+// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+
+// 3 - Crie uma função para mostrar o tamanho de um objeto.
+
+
+// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+
+// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3.
