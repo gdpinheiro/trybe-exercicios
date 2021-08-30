cc209d6b82d44ee52c580229e59b6a78cae5803f
Giovanni Duarte <gikigiio@gmail.com>
2021-08-30 17:54:45 -0300

    Pt1E1

--- modulo-1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/Pt1.js
+++ modulo-1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/Pt1.js
@@ -34,12 +34,25 @@ const order = {
   },
 };
 
+const orderAddress = order['address']
+const orderPizza = order['order']['pizza']
+const orderDrinks = order['order']['drinks']
+const orderDelivery = order['order']['delivery']
+const orderPayment = order['payment']
+
+// Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  let cInfoLog = `Olá Ana Silveira, entrega para: ${order['name']}, Telefone: ${order['phoneNumber']}, R. ${orderAddress['street']}, Nº: ${orderAddress['number']}, AP: ${orderAddress['apartment']}`
+  console.log(cInfoLog)
 }
 
 customerInfo(order);
+
+// Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+//  - Modifique o nome da pessoa compradora.
+//  - Modifique o valor total da compra para R$ 50, 00.
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
