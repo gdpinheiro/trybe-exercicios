1397f9ebb58accd37bf12567710a1e298784adf3
Giovanni Duarte <gikigiio@gmail.com>
2021-08-30 19:11:38 -0300

    Pt2E6

--- modulo-1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/Pt2.js
+++ modulo-1-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2/Pt2.js
@@ -19,33 +19,41 @@ const lesson3 = {
 };
 
 // 1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
 const addKeyValue = (object, key, value) => {
   let newKey = key
   object[newKey] = value
 }
 
 addKeyValue(lesson2, 'turno', 'manhã')
+
 // 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
 const listKeys = object => {
   let keyList = Object.keys(object)
   // console.log(keyList);
 }
 
 listKeys(lesson2)
+
 // 3 - Crie uma função para mostrar o tamanho de um objeto.
+
 const objectSize = object => {
   let obSize = Object.keys(object).length
   // console.log(obSize);
 }
 
 objectSize(lesson2)
+
 // 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
 const listValues = object => {
   let valuesList = Object.values(object)
   // console.log(valuesList);
 }
 
 listValues(lesson2)
+
 // 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3.
 
 const al1 = {}
@@ -58,4 +66,23 @@ addKeyValue(al3, 'lesson3', Object.entries(lesson3))
 
 const allLessons = Object.assign({}, al1, al2, al3)
 
-console.log(allLessons);
+// console.log(allLessons);
+
+// 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const sumStudents = () => {
+  const lessons = Object.entries(allLessons)
+  let result = 0;
+  for (index in lessons) {
+    const nStud = lessons[index][1][1][1]
+    result += nStud
+  }
+  console.log(result);
+}
+
+sumStudents()
+
+// 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+
+
+// 8 - Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
