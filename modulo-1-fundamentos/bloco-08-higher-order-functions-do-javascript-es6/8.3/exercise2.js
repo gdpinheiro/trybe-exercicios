2e17eabfd37994dd8555d3ae15bf3e7349055e07
Giovanni Duarte <gikigiio@gmail.com>
2021-09-08 18:38:24 -0300

    Ex 2

--- modulo-1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.3/exercise2.js
+++ modulo-1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.3/exercise2.js
@@ -65,9 +65,6 @@ const books = [
 
 // Adicione o código do exercício aqui:
 
-// Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
-// Dica: use as funções map , sort
-
 const expectedResult = [
   {
     age: 31,
@@ -95,8 +92,17 @@ const expectedResult = [
   },
 ];
 
+// Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// Dica: use as funções map , sort
+
 function nameAndAge() {
-  // escreva seu código aqui
+  let result = books.map((elem) => {
+    return {
+      age: elem.releaseYear - elem.author.birthYear,
+      author: elem.author.name,
+    };
+  });
+  return result.sort((a, b) => a.age - b.age);
 }
 
 assert.deepStrictEqual(nameAndAge(), expectedResult);
