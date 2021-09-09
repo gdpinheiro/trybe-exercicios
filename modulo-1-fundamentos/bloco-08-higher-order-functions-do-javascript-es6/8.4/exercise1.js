8867e2ac94da7f0dc86a34467f0c3c2ee93a38a9
Giovanni Duarte <gikigiio@gmail.com>
2021-09-09 18:40:57 -0300

    Ex 1

--- modulo-1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.4/exercise1.js
+++ modulo-1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.4/exercise1.js
@@ -5,7 +5,7 @@ const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
 // Dada uma matriz, transforme em um array.
 
 function flatten() {
-  // escreva seu código aqui
+  return arrays.reduce((acc, cur) => acc.concat(cur));
 }
 
 assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
