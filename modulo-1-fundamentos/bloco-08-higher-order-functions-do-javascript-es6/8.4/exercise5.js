dbc6365a2b25d77fe37a4fe987f59e834da3704b
Giovanni Duarte <gikigiio@gmail.com>
2021-09-09 20:12:54 -0300

    Ex 5 Não finalizado

--- modulo-1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.4/exercise5.js
+++ modulo-1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/8.4/exercise5.js
@@ -12,10 +12,16 @@ const names = [
   "Alarucha",
 ];
 
-// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+// Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra A maiúscula ou minúscula.
 
 function containsA() {
-  // escreva seu código aqui
+  let result = names.reduce((acc, cur, index) => {
+    // console.log(acc);
+    // console.log(cur);
+    // console.log(index);
+  }, 0);
+  // console.log(result);
+  return result;
 }
 
 assert.deepStrictEqual(containsA(), 20);
