13134e2735a78a32523e406b1a78e2229a6d3887
Giovanni Duarte <gikigiio@gmail.com>
2021-09-22 18:45:49 -0300

    getUserNamePromise

--- modulo-1-fundamentos/bloco-10-testes-automatizados-com-jest/10.2/getUserNamePromise.test.js
+++ modulo-1-fundamentos/bloco-10-testes-automatizados-com-jest/10.2/getUserNamePromise.test.js
@@ -15,3 +15,16 @@ const findUserById = (id) =>
   });
 
 const getUserName = (userId) => findUserById(userId).then((user) => user.name);
+
+describe("Tests the getUserName function", () => {
+  it("checks the function callback", (done) => {
+    try {
+      getUserName(1).then((result) => {
+        expect(result).toBe("Mark");
+      });
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
