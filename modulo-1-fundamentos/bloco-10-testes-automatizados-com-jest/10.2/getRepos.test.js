8be2759f61ff2ff579854c2b930d9c0c3ab378b4
Giovanni Duarte <gikigiio@gmail.com>
2021-09-22 19:18:31 -0300

    getRepos não finalizado

--- modulo-1-fundamentos/bloco-10-testes-automatizados-com-jest/10.2/getRepos.test.js
+++ modulo-1-fundamentos/bloco-10-testes-automatizados-com-jest/10.2/getRepos.test.js
@@ -7,3 +7,17 @@ const getRepos = (url) => {
       return data.map((repo) => repo.name);
     });
 };
+
+// "sd-01-week4-5-project-todo-list"
+// "sd-01-week4-5-project-meme-generator"
+
+describe("Tests the getRepos function", () => {
+  it("checks if the repos are contained", async () => {
+    const url = "https://api.github.com/orgs/tryber/repos";
+
+    return getRepos(url).then((result) => {
+      expect(result).toContain("sd-01-week4-5-project-todo-list");
+      expect(result).toContain("sd-01-week4-5-project-meme-generator");
+    });
+  });
+});
