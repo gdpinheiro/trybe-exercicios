a62e5e653d5b0ee3efd7931f2ca1488edecf31a1
Giovanni Duarte <gikigiio@gmail.com>
2021-10-07 15:33:10 -0300

    Arquivos iniciais Ex 11.1

---
+++ modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/12.1/my-pokedex/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Pokedex from "./Pokedex";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokedex</h1>
+      <section className="pokedex">
+        <Pokedex />
+      </section>
+    </div>
+  );
+}
+
+export default App;
