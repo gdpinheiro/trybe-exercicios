89ddb43fb529c3011f15018078e17f1fe63b9b68
black <gikigiio@gmail.com>
2021-10-09 15:38:02 -0300

    Pausa 09/10

--- modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/12.2/my-form-2.0/src/App.js
+++ modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/12.2/my-form-2.0/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          Envie seu currículo
           <Form />
         </header>
       </div>
