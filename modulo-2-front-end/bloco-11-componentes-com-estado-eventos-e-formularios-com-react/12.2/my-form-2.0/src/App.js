97650197f5396bba5fe94e3b3fb41b02f2c4a11f
Giovanni Duarte <gikigiio@gmail.com>
2021-10-09 12:15:22 -0300

    Reincício do Exercício

--- modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/12.2/my-form-2.0/src/App.js
+++ modulo-2-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/12.2/my-form-2.0/src/App.js
@@ -1,7 +1,4 @@
 import "./App.css";
-import PersonalData from "./Components/PersonalData";
-import LastJob from "./Components/LastJob";
-import Button from "./Components/Button";
 import React, { Component } from "react";
 
 class App extends Component {
@@ -9,12 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <PersonalData />
-          <LastJob />
-          <div>
-            <Button>Enviar</Button>
-            <Button>Limpar</Button>
-          </div>
+          
         </header>
       </div>
     );
